e101bb1b34967c7899f0be7b73619c24cc0dfec5
gxrlxv <gorlov1207@gmail.com>
2022-08-02 13:55:23 +0300

    init useCase

--- track_service/internal/usecase/track.go
+++ track_service/internal/usecase/track.go
@@ -1,1 +1,29 @@
 package usecase
+
+import (
+	"context"
+	v1 "github.com/gxrlxv/musique/music_service/api/track/v1"
+)
+
+type trackUseCase struct {
+}
+
+func NewTrackUseCase() *trackUseCase {
+	return &trackUseCase{}
+}
+
+func (tu *trackUseCase) AddTrack(ctx context.Context, trackId string) (*v1.AddTrackReply, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (tu *trackUseCase) RemoveTrack(ctx context.Context, trackId string) (*v1.RemoveTrackReply, error) {
+	//TODO implement me
+
+	panic("implement me")
+}
+
+func (tu *trackUseCase) GetTracks(ctx context.Context) (*v1.GetTracksReply, error) {
+	//TODO implement me
+	panic("implement me")
+}
