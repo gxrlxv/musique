2ee14a1459a518a7573c2168e8ed75cdc2a8a9db
gxrlxv <gorlov1207@gmail.com>
2022-08-02 17:49:12 +0300

    deleted unused entities

--- auth_service/internal/domain/session.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "time"
-
-type Session struct {
-	UserId       string    `json:"user_id"`
-	RefreshToken string    `json:"refresh_token"`
-	ExpiresAt    time.Time `json:"expires_at"`
-}
