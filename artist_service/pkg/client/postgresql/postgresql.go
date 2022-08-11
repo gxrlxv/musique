5bb0c984030ed2987c064bace2f7600173f2f286
gxrlxv <gorlov1207@gmail.com>
2022-08-11 14:25:31 +0300

    updated postgre client

--- artist_service/pkg/client/postgresql/postgresql.go
+++ artist_service/pkg/client/postgresql/postgresql.go
@@ -17,7 +17,6 @@ type Client interface {
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-	Begin(ctx context.Context) (pgx.Tx, error)
 }
 
 func NewClient(ctx context.Context, maxAttempts int, sc config.StorageConfig) (pool *pgxpool.Pool, err error) {
